0d4793493a1c7d54f4e9c616c8c753248f5450f4
Katie West <katiewest820@gmail.com>
2018-02-17 11:40:46 -0800

    more testing

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(
   })
 )
 
+app.get('/', (req, res) => {
+  res.sendFile(_dirname + '../baker-and-spice-react/public/index.html').status(200);
+});
+
 //app.use('/static', express.static(path.join('public')))
 app.use('/images', express.static(path.join(__dirname, 'public')))
 //app.use('/images', express.static(__dirname + '/public'))
@@ -31,10 +35,6 @@ app.use('/auth', authRoutes);
 app.use('/recipe', recipeRoutes);
 app.use('/pantry', pantryRoutes);
 app.use('/apiRequest', apiRequest);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(_dirname + '/public/index.html')).status(200);
-});
 
 let server;
 
